9ee802963e360b67a7c4b4397bf45e53efd2576b
Andre Arko <andre@arko.net>
2013-05-03 13:22:46 -0700

    escaped single quotes? really?

--- spec/connection_decoration_spec.rb
+++ spec/connection_decoration_spec.rb
@@ -10,19 +10,19 @@ describe 'Makara Connection Decoration' do
   let(:config){ simple_config }
   let(:con){ adapter.mcon }
 
-  it 'should integrate with the underlying adapters, giving reference to the makara adapter' do
+  it "should integrate with the underlying adapters, giving reference to the makara adapter" do
     con.should respond_to(:with_makara)
   end
 
   describe '#with_makara' do
 
-    it 'should return the makara adapter if we haven\'t hijacked the execution yet' do
+    it "should return the makara adapter if we haven't hijacked the execution yet" do
       con.with_makara do |acceptor|
         acceptor.should eql(adapter)
       end
     end
 
-    it 'should return nil if we\'ve already hijacked the execution' do      
+    it "should return nil if we've already hijacked the execution" do
       adapter.send(:hijacking!) do
         con.with_makara do |acceptor|
           acceptor.should be_nil
@@ -32,7 +32,7 @@ describe 'Makara Connection Decoration' do
 
   end
 
-  it 'should invoke the makara adapter when execute is called internally' do
+  it "should invoke the makara adapter when execute is called internally" do
     adapter.should_receive(:execute).with('select * from users').once
     con.execute('select * from users')
   end
